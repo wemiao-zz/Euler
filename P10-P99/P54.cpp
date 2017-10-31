a2c5f5304113b3d559c920779330bca8444ddbfb
William Miao <404.error.not.found@gmail.com>
2017-10-30 21:53:31 -0700

    partially complete P54

--- P10-P99/P54.cpp
+++ P10-P99/P54.cpp
@@ -2,29 +2,63 @@
 
 using namespace std;
 
-int main() {
-    vector<vector<unsigned long>> pascal;
-    vector<unsigned long> tmp = vector<unsigned long>(2,1);
-    pascal.emplace_back(tmp);
-    int cnt = 0;
-    for(int i=0; i < 99; i++) {
-        tmp = vector<unsigned long>(i+3,0);
-        tmp[0] = 1; // edges of pascal's triangle
-        tmp[tmp.size() - 1] = 1; // edges of pascal's triangle
-        for(int j=1; j < tmp.size()-1; j++) {
-           tmp[j] = pascal[i][j-1] + pascal[i][j]; 
-           if(tmp[j] > 1000000) cnt++;
-        }
-        pascal.emplace_back(tmp);
+bool isWin(bitset<52> h1, bitset<52> h2) {
+    string flush = "1111111111111";
+    bitset<13> checkFlush(flush);
 
+
+
+
+}
+
+int parse(string n) {
+    unordered_map <char,int> suit;
+    suit['C'] = 0;
+    suit['H'] = 13;
+    suit['S'] = 26;
+    suit['D'] = 39;
+
+    unordered_map <char,int> cardNum;
+    cardNum['2'] = 0;
+    cardNum['3'] = 1;
+    cardNum['4'] = 2;
+    cardNum['5'] = 3;
+    cardNum['6'] = 4;
+    cardNum['7'] = 5;
+    cardNum['8'] = 6;
+    cardNum['9'] = 7;
+    cardNum['T'] = 8;
+    cardNum['J'] = 9;
+    cardNum['Q'] = 10;
+    cardNum['K'] = 11;
+    cardNum['A'] = 12;
+    return cardNum[n[0]] + suit[n[1]]; 
+}
+
+int main() {
+    ifstream infile("P54.txt");
+    string line;
+    string n;
+    int cnt;
+    bitset<52> cards1;
+    bitset<52> cards2;
+    int wins = 0;
+    while(getline(infile, line)) {
+        cnt = 0;
+        cards1.reset();
+        cards2.reset();
+        istringstream iss(line);
+        while(iss >> n) {
+            if(cnt < 5) {
+                cards1.set(parse(n)); 
+            } else {
+                cards2.set(parse(n)); 
+            }
+            cnt++;
+        }
+        if(isWin(cards1, cards2)) wins++;
+
+        break; // just test for 1 hand
     }
-    cout << "number over 1 million: " << cnt << '\n';
-    /* for(auto &i: pascal) { */
-    /*     cout << cnt << ": "; */
-    /*     cnt++; */
-    /*     for(auto &j: i) { */
-    /*         cout << j << ' '; */
-    /*     } */
-    /*     cout << '\n'; */
-    /* } */
+    cout << "Total P1 wins: " << wins << '\n';
 }
